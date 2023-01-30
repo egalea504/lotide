60d42349bf003b6a37bff629e5cc0d51ed8d2f64
eliza galea <111540402+egalea504@users.noreply.github.com>
2023-01-29 21:52:40 -0500

    created an assert function that evaluates if code is true or not

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected + " ✅✅✅");
+  } else console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected + " 🛑🛑🛑");
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Light", "Light");
+assertEqual(1,3);
