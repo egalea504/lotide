eca3577c225dc8e7754c41eac79d1cbef3abd5ef
eliza galea <111540402+egalea504@users.noreply.github.com>
2023-02-01 18:23:48 -0500

    function that evaluates if two arrays are equal or not

---
+++ assertArraysEqual.js
@@ -0,0 +1,24 @@
+const eqArrays = function(a, b) {
+  let result = false;
+
+  if (a.length !== b.length) {
+    return false;
+  } else { 
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false;
+    } else result = true;
+  }
+}
+return result;
+}
+
+const assertArraysEqual = function(actual, expected) {
+
+if (eqArrays(actual, expected)) {
+  console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} ✅✅✅`);
+} else console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} 🛑🛑🛑`);
+};
+
+assertArraysEqual([1, 2, 3], [1, 3, 2]);
