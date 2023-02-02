ac0d38285039127a44892c8bb3921af364556ef5
eliza galea <111540402+egalea504@users.noreply.github.com>
2023-02-01 22:59:32 -0500

    processed eslint on the code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -3,22 +3,21 @@ const eqArrays = function(a, b) {
 
   if (a.length !== b.length) {
     return false;
-  } else { 
-
-  for (let i = 0; i < a.length; i++) {
+  } else for (let i = 0; i < a.length; i++) {
     if (a[i] !== b[i]) {
       return false;
     } else result = true;
   }
-}
-return result;
-}
+  return result;
+};
+  
+
 
 const assertArraysEqual = function(actual, expected) {
 
-if (eqArrays(actual, expected)) {
-  console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} ✅✅✅`);
-} else console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} 🛑🛑🛑`);
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected} ✅✅✅`);
+  } else console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected} 🛑🛑🛑`);
 };
 
 assertArraysEqual([1, 2, 3], [1, 3, 2]);
