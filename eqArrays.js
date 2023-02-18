51a1b0fac42ed11b0b1507ab17ec61171e459e4b
eliza galea <111540402+egalea504@users.noreply.github.com>
2023-02-17 19:18:27 -0500

    corrected function to work in empty arrays situation

--- eqArrays.js
+++ eqArrays.js
@@ -8,10 +8,12 @@ const eqArrays = function(a, b) {
   for (let i = 0; i < a.length; i++) {
     if (a[i] !== b[i]) {
       return false;
-    } else result = true;
+    }
   }
 }
-return result;
+return true;
+// corrected my eqArrays function in order to return true if the arrays pass all the if functions instead of returning result
+  // return result which I inputed before caused an error when comparing empty arrays. since the array is empty, no indexes can be compared so it caused the function to always test false - failing the test even if both arrays are []
 }
 
 module.exports = eqArrays;
