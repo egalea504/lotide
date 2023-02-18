73fa8889506776d083a90bd74eefb212cd2e712f
eliza galea <111540402+egalea504@users.noreply.github.com>
2023-02-17 21:55:54 -0500

    updated assert testing functions

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -2,3 +2,4 @@ const assertArraysEqual = require('../assertArraysEqual');
 
 assertArraysEqual([1, 2, 3], [1, 3, 2]); // => false
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -2,7 +2,7 @@
 
 const assertEqual = require('../assertEqual');
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Light", "Light");
-assertEqual(1,3);
+assertEqual("Lighthouse Labs", "Bootcamp"); // => false
+assertEqual(1, 1); // => true
+assertEqual("Light", "Light"); // => true
+assertEqual(1,3); // => false
